f1ceb540a64c60846e5c90b02a631620cc166cb3
Zuhaib Ahmed <zuhaibahmed.bssef24@iba-suk.edu.pk>
2026-08-19 13:33:10 +0500

    Update gradebook.py

--- src/gradebook/gradebook.py
+++ src/gradebook/gradebook.py
@@ -1,1 +1,13 @@
+class Student:
+    def __init__(self, name2, id_number ):
+        self.name = name2
+        self.roll_no = id_number
+        self.scores = []
 
+    def average(self):
+        if not self.scores:
+            return 0.0
+
+        return sum(self.scores) / len(self.scores)
+
+    #  this is the new file where I make changes
