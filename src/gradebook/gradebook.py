1518250df7cfe218fb38e57e15098c697d6c5ded
zuhaibahmed347 <zuhaibahmed.bssef24@iba-suk.edu.pk>
2026-08-20 16:08:39 +0500

    Rename roll_no to student_id

--- src/gradebook/gradebook.py
+++ src/gradebook/gradebook.py
@@ -1,7 +1,7 @@
 class Student:
-    def __init__(self, name, roll_no, scores):
+    def __init__(self, name, student_id, scores):
         self.name = name
-        self.roll_no = roll_no
+        self.student_id = student_id
         self.scores = scores
 
     def add_score(self, student_score):
